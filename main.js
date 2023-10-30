4787eec92c3f1cc99efab5fa8a69f6deda23603a
Koka224 <148534808+Koka224@users.noreply.github.com>
2023-10-30 15:55:02 +0600

    exercise 3

--- main.js
+++ main.js
@@ -7,9 +7,19 @@
 // console.log(result);
 
 // 2) Дан массив с числами. Числа могут быть положительными и отрицательными. Найдите сумму положительных элементов массива.
-const sum = [-1, 2, -3, 4, 5];
-let result = 0;
-for (i = 0; i < sum.length; i++) {
-  if (sum[i] > 0) result = result + sum[i];
+// const sum = [-1, 2, -3, 4, 5];
+// let result = 0;
+// for (i = 0; i < sum.length; i++) {
+//   if (sum[i] > 0) result = result + sum[i];
+// }
+// console.log(result);
+
+// 3) Дан массив с элементами [1, 2, 5, 9, 4, 13, 4, 10]. С помощью цикла for и оператора if проверьте есть ли в массиве элемент со значением, равным
+// 4. Если есть - выведите на экран 'Есть!' и выйдите из цикла. Если нет - ничего делать не надо.
+const sumArr = [1, 2, 5, 9, 4, 13, 4, 10];
+for (let i = 0; i < sumArr.length; i++) {
+  if (sumArr[i] === 4) {
+    console.log("Есть");
+    break;
+  }
 }
-console.log(result);
